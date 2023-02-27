4d841e0ba2e4ad81002456950f740d0b7e1c78f4
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2023-02-27 13:21:13 +0000

    Update pinata.js

--- src/pinata.js
+++ src/pinata.js
@@ -4,15 +4,15 @@ const secret = process.env.REACT_APP_PINATA_SECRET;
 
 const axios = require('axios');
 const FormData = require('form-data');
-
+const JWT = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIwNDdiYmFjZC1lYzEyLTQ1Y2UtOTYxZS03MTMxYjM5NGE1YTIiLCJlbWFpbCI6ImNoaWtvY2hhaW5uZXR3b3JrQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImlkIjoiRlJBMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfSx7ImlkIjoiTllDMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiJlMDhhYTkyN2I5NzVkN2I3NTU3ZSIsInNjb3BlZEtleVNlY3JldCI6IjNiMWIxNzEzMDVjNDdkYWU2YzBkZTVlYWVmMWFjYTZkZGI1YTExOGQ1YjU0MjU2Njg0MTU5NzcyYzA0OWFkY2UiLCJpYXQiOjE2Nzc0NTA1MDl9._s43tAGrRXMLxjJa-t2NO6eUk1wQKvnQShnbxmrhdfc'
 export const uploadJSONToIPFS = async(JSONBody) => {
     const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
     //making axios POST request to Pinata ⬇️
     return axios 
         .post(url, JSONBody, {
             headers: {
-                pinata_api_key: key,
-                pinata_secret_api_key: secret,
+                pinata_api_key: "e08aa927b975d7b7557e",
+                pinata_secret_api_key: "3b1b171305c47dae6c0de5eaef1aca6ddb5a118d5b54256684159772c049adce",
             }
         })
         .then(function (response) {
@@ -41,7 +41,7 @@ export const uploadFileToIPFS = async(file) => {
     const metadata = JSON.stringify({
         name: 'testname',
         keyvalues: {
-            exampleKey: 'exampleValue'
+            exampleKey: 'example'
         }
     });
     data.append('pinataMetadata', metadata);
@@ -69,9 +69,10 @@ export const uploadFileToIPFS = async(file) => {
             maxBodyLength: 'Infinity',
             headers: {
                 'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
-                pinata_api_key: key,
-                pinata_secret_api_key: secret,
-            }
+                pinata_api_key: "e08aa927b975d7b7557e",
+                pinata_secret_api_key: "3b1b171305c47dae6c0de5eaef1aca6ddb5a118d5b54256684159772c049adce",
+                Authorization: JWT
+              }
         })
         .then(function (response) {
             console.log("image uploaded", response.data.IpfsHash)
